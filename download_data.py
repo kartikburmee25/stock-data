a9cdace744dc2881916edafcc71aaca13b78766b
Kartik Burmee <kartik@Kartiks-MacBook-Air.local>
2021-12-19 19:19:44 -0800

    Adding nasdaq 100 list and download logic.

--- download_data.py
+++ download_data.py
@@ -7,87 +7,50 @@ import yfinance as yf
 
 # Connecting to database
 
-#db_password = os.environ.get('DB_PASSWORD')
-db_password = '123456'
+db_password = os.environ.get('DB_PASSWORD')
+
 engine = create_engine('postgresql://postgres:{}@localhost/stock-data'.format(db_password))
 
 # Inserting pandas dataframe for a stock into DB as a new table
-def create_prices_table(symbol, period):
+def store_prices_data(symbol, period, table_name):
 
     df = yf.download(symbol,period=period)
+    df.insert(0, 'Symbol', symbol)
     df.reset_index(inplace=True)
     df = df.rename(columns = {'index':'Date'})
 
+    print(df)
+
     # writing the data into database
-    df.to_sql('daily_prices', con=engine, schema='public', if_exists='replace', index=False)
-
-    # Create a primary key on the table
-    query = """ALTER TABLE daily_prices 
-              ADD PRIMARY KEY (Date);"""
-    engine.execute(query)
-    
-    return 'Daily prices table created'
+    df.to_sql(table_name, con=engine, schema='public', if_exists='append', index=False)
+ 
+    return
 
 
-# function to insert data into existing prices table
-def import_stock_data_file(symbol):
-    path = bars_path + '/{}.csv'.format(symbol)
-    df = pd.read_csv(path, index_col=[0], parse_dates=[0])
+# function to insert data into existing prices table from csv file
+def import_stock_data_file(file_path, table_name):
     
-    # Some clean up for missing data
-    if 'dividend' not in df.columns:
-        df['dividend'] = 0
-    df = df.fillna(0.0)
+    df = pd.read_csv(file_path)
     
-    # First part of the insert statement
-    insert_init = """INSERT INTO daily_prices
-                    (symbol, date, volume, open, close, high, low, dividend, ratio_adj,
-                    volume_adj, open_adj, close_adj, high_adj, low_adj, dollar_volume)
-                    VALUES
-                """
-                
-    # Add values for all days to the insert statement
-    vals = ",".join(["""('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', 
-                     '{}', '{}', '{}')""".format(
-                     symbol,
-                     date,
-                     row.volume,
-                     row.open,
-                     row.close,
-                     row.high,
-                     row.low,
-                     row.dividend,
-                     row.ratio_adj,
-                     row.volume_adj,
-                     row.open_adj,
-                     row.close_adj,
-                     row.high_adj,
-                     row.low_adj,
-                     row.dollar_volume
-                     ) for date, row in df.iterrows()])
-    
-    # Handle duplicate values - Avoiding errors if you've already got some data in your table
-    insert_end = """ ON CONFLICT (symbol, date) DO UPDATE 
-                SET
-                volume = EXCLUDED.volume,
-                open = EXCLUDED.open,
-                close = EXCLUDED.close,
-                high = EXCLUDED.high,
-                low = EXCLUDED.low,
-                dividend = EXCLUDED.dividend,
-                ratio_adj = EXCLUDED.ratio_adj,
-                volume_adj = EXCLUDED.volume_adj,
-                open_adj = EXCLUDED.open_adj,
-                close_adj = EXCLUDED.close_adj,
-                high_adj = EXCLUDED.high_adj,
-                low_adj = EXCLUDED.low_adj,
-                dollar_volume =  EXCLUDED.dollar_volume;
-                """
+    # writing the data into database
+    df.to_sql(table_name, con=engine, schema='public', if_exists='append', index=False)
 
-    # Put together the query string
-    query = insert_init + vals + insert_end
-    
-    # Fire insert statement
-    engine.execute(query)
+    return
 
-#create_prices_table('AAPL','1y')
+
+def parse_index_symbols(index_name):
+
+	df = pd.read_csv(index_name + '.csv')
+	symbol_list = df['Symbol'].tolist()
+
+	return symbol_list
+
+
+if __name__ == '__main__':
+
+	# download and store nasdaq 100 data for past 5 years
+
+	nasdaq_100 = parse_index_symbols('nasdaq_100')
+	for symbol in nasdaq_100:
+		store_prices_data(symbol, '5y')
+
